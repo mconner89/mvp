9c3129550113de993ff8f45b93de64dd3e0206f6
mconner89 <john.conner@eagles.usm.edu>
2020-09-21 12:56:26 -0500

    web pack

--- index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const { PORT } = process.env;
-
-app.listen(PORT, () => console.info(`App listening on http://localhost:${PORT}`));
-
-app.get('/', (req, res) => res.send('Hooray!'));
---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { PORT } = process.env;
+
+app.listen(PORT, () => console.info(`App listening on http://localhost:${PORT}`));
+
+app.get('/', (req, res) => res.send('Hooray!'));
+
+app.get('/test', (req, res) => res.send('idk man'));
