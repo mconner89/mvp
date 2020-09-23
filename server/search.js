1fbb84e5bbd4a11bc2e922326ae716261147c777
mconner89 <john.conner@eagles.usm.edu>
2020-09-22 20:14:34 -0500

    mvp

--- server/search.js
+++ server/search.js
@@ -11,4 +11,26 @@ const getPuuid = (username) => {
   return axios(options);
 };
 
+const getMatches = (puuid) => {
+  const options = {
+    url: `https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/${puuid}/ids?count=20`,
+    headers: {
+      'X-Riot-Token': `${API_KEY}`
+    }
+  };
+  return axios(options);
+};
+
+const getMatchData = (matchId) => {
+  const options = {
+    url: `https://americas.api.riotgames.com/tft/match/v1/matches/${matchId}`,
+    headers: {
+      'X-Riot-Token': `${API_KEY}`
+    }
+  };
+  return axios(options);
+}
+
 module.exports.getPuuid = getPuuid;
+module.exports.getMatches = getMatches;
+module.exports.getMatchData = getMatchData;
